e707ca2e128cd058f49cc78e831656cea94bba4b
Mathos6 <kanijeremie06@gmail.com>
2025-11-17 18:32:17 +0100

    Externalisation des variables ENV

--- src/stucollab/settings/settings_prod.py
+++ src/stucollab/settings/settings_prod.py
@@ -156,7 +156,6 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'stucollab.app@gmail.com'
-EMAIL_HOST_PASSWORD = 'xkel lmjg qboo rwpg'
-DEFAULT_FROM_EMAIL = 'stucollab.app@gmail.com'
-
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.environ.get("DEFAULT_FROM_EMAIL")
