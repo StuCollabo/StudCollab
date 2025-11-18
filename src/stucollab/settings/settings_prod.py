d01a5ae98b88849bdb500af6c6fbd76ab32f2f18
Mathos6 <kanijeremie06@gmail.com>
2025-11-18 23:00:09 +0100

    Passage au port 2525 pour les mails

--- src/stucollab/settings/settings_prod.py
+++ src/stucollab/settings/settings_prod.py
@@ -154,7 +154,7 @@ LOGIN_URL = "/me/signin_signup"
 # Pour la production (exemple avec Gmail)
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 2525
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
